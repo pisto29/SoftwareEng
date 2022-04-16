f001c64471c4dce766a7828b69ade8aa5bf07545
Digimatt <mattiadigiovanni98@gmail.com>
2022-04-16 11:28:47 +0200

    metodi interfaccia

--- Entity/EffettoAnnullaMossa.java
+++ Entity/EffettoAnnullaMossa.java
@@ -5,7 +5,10 @@ public class EffettoAnnullaMossa implements EffettoComposite {
     public void ApplicaEffetto(Personaggio p1){
 
         p1.DisabiltaAttacco();
+        
+
     }
 
+    
     
 }
--- Entity/EffettoComposite.java
+++ Entity/EffettoComposite.java
@@ -3,6 +3,8 @@ package Entity;
 public interface EffettoComposite {
 
     public void ApplicaEffetto(Personaggio p1);
-
-    
+    public void Add(EffettoComposite e);
+    public void Remove(EffettoComposite e);
+    public int getChild(int a);
+ 
 }
