1c28062471ca6e3c9a363f18770cd5a9c3cb94b3
pisto29 <riccardo.pistilli@hotmail.it>
2022-05-18 11:43:26 +0200

    g14

--- Entity/Abilità.java
+++ Entity/Abilità.java
@@ -1,10 +1,19 @@
 package Entity;
 
+
+
 public interface Abilità {
-    
+
+   
+
     public void Attivazione(Personaggio utilizzatore,Personaggio bersaglio);
+
     public Fase getFase_attivazione();
+
     //hghgjkb
+
     public void a();
+
     public void b();
+
 }
