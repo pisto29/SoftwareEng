f503350c09f07d17c68523fd029ae1b2242e5c0e
alessandro <alestefa98@gmail.com>
2022-05-30 12:36:24 +0200

    mossacambio

--- Entity/MossaCambio.java
+++ Entity/MossaCambio.java
@@ -1,5 +1,12 @@
 package Entity;
 
 public class MossaCambio implements EsecuzioneTurno {
+
+    @Override
+    public void esecuzione(Turno t) {
+       t.getPersonaggi().get(1).Sostituzione();
+       t.getSostituzioni().get(1).Sostituzione();
+       t.getPersonaggi().add(1, t.getSostituzioni().get(1)); 
+    }
     
 }
