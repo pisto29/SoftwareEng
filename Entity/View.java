a3c589d1a08e4294ffbba897fd53ad90858f492f
pisto29 <riccardo.pistilli@hotmail.it>
2022-09-02 17:38:30 +0200

    Mattia in Discoteca con visual studio sul portatile

--- Entity/View.java
+++ Entity/View.java
@@ -1,5 +1,7 @@
 package Entity;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -226,6 +228,32 @@ return scelta;
         return false;
        }
 
+    }
+
+
+    public void SceltaRegolamento(){
+        System.out.println("Seleziona un Regolamento: ");
+        ArrayList<String> results = new ArrayList<String>();
+
+        String s = new File(".").getAbsolutePath()+"/Foundation/file/Regolamento/";
+
+ 
+        File[] files = new File(s).listFiles();
+        //If this pathname does not denote a directory, then listFiles() returns null. 
+
+        for (File file : files) {
+            if (file.isFile()) {
+                results.add(file.getName());
+            }   
+        }
+        for(String r:results){
+            System.out.println(r);
+        }
+    }
+
+    public static void main(String[] args) {
+        View v = new View();
+        v.SceltaRegolamento();
     }
     
 }
